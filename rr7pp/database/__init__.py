94c8dd49eb34d8b2d30952a89a7efe4f9b58c395
MUHAMMAD <104639251+R0R77@users.noreply.github.com>
2022-06-20 19:01:14 +0300

    Update __init__.py

--- rr7pp/database/__init__.py
+++ rr7pp/database/__init__.py
@@ -1,9 +1,9 @@
-from rr7ppx.database.postgres.afk_sql import AFKSQL
-from rr7ppx.database.postgres.notes_sql import NOTESSQL
-from rr7ppx.database.postgres.pmpermit_sql import PMPERMITSQL
-from rr7ppx.database.postgres.dv_sql import DVSQL
-from rr7ppx.database.postgres.welcome_sql import WELCOMESQL
-from rr7ppx.database.postgres.filters_sql import FILTERSSQL
+from rr7pp.database.postgres.afk_sql import AFKSQL
+from rr7pp.database.postgres.notes_sql import NOTESSQL
+from rr7pp.database.postgres.pmpermit_sql import PMPERMITSQL
+from rr7pp.database.postgres.dv_sql import DVSQL
+from rr7pp.database.postgres.welcome_sql import WELCOMESQL
+from rr7pp.database.postgres.filters_sql import FILTERSSQL
 
 
 
